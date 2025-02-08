b38c43466f798efe4a5345f283f79fe6d349cc5d
tharnyelilvannan <tharny2005@outlook.com>
2025-02-08 14:02:35 -0500

    Initial flyForward method (close #2)

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
@@ -0,0 +1,5 @@
+package main.java.ca.mcmaster.se2aa4.island.teamXXX;
+
+public enum Direction {
+    NORTH, SOUTH, EAST, WEST
+}
---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Fly.java
@@ -0,0 +1,18 @@
+package main.java.ca.mcmaster.se2aa4.island.teamXXX;
+
+class Fly {
+    public void flyForward(Direction direction, LocationCoordinates currentLocation) {
+        if (direction == Direction.NORTH) {
+            // update coordinates, depends on implementation of currentLocation
+        }
+        else if (direction == Direction.SOUTH) {
+            // update coordinates, depends on implementation of currentLocation
+        }
+        else if (direction == Direction.WEST) {
+            // update coordinates, depends on implementation of currentLocation
+        }
+        else if (direction == Direction.EAST) {
+            // update coordinates, depends on implementation of currentLocation
+        }
+    }
+}
