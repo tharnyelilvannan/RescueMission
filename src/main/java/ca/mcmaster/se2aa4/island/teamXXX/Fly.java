15b014688397f0b0867af1532b2bc87c3acc2f20
Senuni Kavisinghe <senuni.05@gmail.com>
2025-02-09 20:23:26 -0500

    closes #4: Implement flyLeft() method

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Fly.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class Fly {
+    public void flyLeft(Direction direction, LocationCoordinates currentLocation) {
+    if (direction == Direction.NORTH) {         // North --> West
+        currentLocation.setX(currentLocation.getX() - 1); 
+    } else if (direction == Direction.SOUTH) {  // South --> East
+        currentLocation.setX(currentLocation.getX() + 1);  
+    } else if (direction == Direction.EAST) {   // East --> North
+        currentLocation.setY(currentLocation.getY() - 1);  
+    } else if (direction == Direction.WEST) {   // West --> South
+        currentLocation.setY(currentLocation.getY() + 1);  
+    }
+    }
+}
