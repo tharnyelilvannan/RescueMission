edb1420477afa1803231866ddff0d5ef1b111ab0
tharnyelilvannan <tharny2005@outlook.com>
2025-02-13 20:02:29 -0500

    Fixed variable

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/LocationCoordinates.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/LocationCoordinates.java
@@ -2,5 +2,5 @@ package main.java.ca.mcmaster.se2aa4.island.teamXXX;
 import java.util.ArrayList;
 
 abstract class LocationCoordinates {
-    ArrayList<Integer, Integer> coordinates = new ArrayList<>();
+    ArrayList<Integer> coordinates = new ArrayList<>();
 }
