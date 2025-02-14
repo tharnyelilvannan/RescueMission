5914b8b0aa83f1acb16a3ea68a6673030ae7bce4
tharnyelilvannan <tharny2005@outlook.com>
2025-02-13 20:00:33 -0500

    Added import

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/LocationCoordinates.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/LocationCoordinates.java
@@ -1,4 +1,5 @@
 package main.java.ca.mcmaster.se2aa4.island.teamXXX;
+import java.util.ArrayList;
 
 abstract class LocationCoordinates {
     ArrayList<Integer, Integer> coordinates = new ArrayList<>();
