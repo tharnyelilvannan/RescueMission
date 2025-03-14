68a34348a511bd73d988fa547221a4edb0a23048
megs <rogerm21@mcmaster.ca>
2025-03-14 15:21:12 -0400

    created algorithm to find land, (close #21)

--- src/main/java/ca/mcmaster/se2aa4/island/team01/GroundDetector.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team01/GroundDetector.java
@@ -15,24 +15,32 @@ public class GroundDetector {
     private Direction currentDirection; // hardcoded
     private int range;
 
-
-
-
-    boolean echoLeft; 
-    boolean echoRight; 
+    // boolean echoLeft; 
+    // boolean echoRight; 
 
     public GroundDetector(Direction direction){
         groundFound = false;
-        mustTurn = false; 
         canLand = false; 
 
-        echoLeft = false; 
-        echoRight = false; 
-
+        // echoLeft = false; 
+        // echoRight = false; 
         currentDirection = direction; 
     }
 
-    public String checkGround() {
+    // public String checkGround() {
+    //     JSONObject request = new JSONObject();
+    //     request.put("action", "echo"); // do echo action
+
+    //     JSONObject parameters = new JSONObject();
+    //     parameters.put("direction", currentDirection.getValue()); 
+
+    //     request.put("parameters", parameters);
+    //     logger.info("Echo request: " + request.toString());
+
+    //     return request.toString();
+    // }
+    
+    public String echoStraight() {
         JSONObject request = new JSONObject();
         request.put("action", "echo"); // do echo action
 
@@ -45,9 +53,35 @@ public class GroundDetector {
         return request.toString();
     }
 
-    public String changeHeading(){
-        logger.debug("Setting must turn to false"); 
-        mustTurn = false; 
+    public String echoLeftWing() {
+        JSONObject request = new JSONObject();
+        request.put("action", "echo"); // do echo action
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("direction", currentDirection.turnLeft().getValue()); 
+
+        request.put("parameters", parameters);
+        logger.info("Echo request: " + request.toString());
+
+        return request.toString();
+    }
+
+    public String echoRightWing() {
+        JSONObject request = new JSONObject();
+        request.put("action", "echo"); // do echo action
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("direction", currentDirection.turnRight().getValue()); 
+
+        request.put("parameters", parameters);
+        logger.info("Echo request: " + request.toString());
+
+        return request.toString();
+    }
+
+    // change heading given a direction
+    public String changeHeading(Direction direction){
+        currentDirection = direction; 
 
         JSONObject request = new JSONObject();
         request.put("action", "heading"); // do heading action
@@ -61,7 +95,7 @@ public class GroundDetector {
         return request.toString();
     }
 
-
+    // fly
     public String fly(){
         JSONObject request = new JSONObject();
         request.put("action", "fly"); // do fly action
@@ -69,6 +103,8 @@ public class GroundDetector {
         logger.info("Fly request:" + request.toString()); 
         return request.toString();
     }
+
+    // scan
     public String scan(){
         JSONObject request = new JSONObject();
         request.put("action", "scan"); // do scan action
@@ -76,53 +112,7 @@ public class GroundDetector {
         logger.info("Scan request:" + request.toString()); 
         return request.toString();
     }
-
-
-
-
-    public String echoDifferentDirection(){
-        JSONObject request = new JSONObject();
-        request.put("action", "echo"); // do echo action
-
-
-        // if both directions have been checked already
-        if (echoLeft == true && echoRight == true && mustTurn == false){
-            logger.debug("Flying one step"); 
-            mustTurn = true;
-            // fly forward one step
-            return fly();
-        }
-        else if (echoLeft == true && echoRight == false){ // to echo left
-            echoLeft = false; 
-            echoRight = true;
-            // echo left
-            JSONObject parameters = new JSONObject();
-            parameters.put("direction", currentDirection.turnLeft().getValue()); 
-            request.put("parameters", parameters);
-            logger.info("Echo request: " + request.toString());
-            return request.toString();
-        }
-        else if (echoLeft == false && echoRight == true){ // to echo right
-            echoLeft = true; 
-            // echo right
-            JSONObject parameters = new JSONObject();
-            parameters.put("direction", currentDirection.turnRight().getValue());
-            request.put("parameters", parameters);
-            logger.info("Echo request: " + request.toString());
-            return request.toString(); 
-        }
-        else{ 
-            // if nothing has been checked
-            echoLeft = true; 
-            JSONObject parameters = new JSONObject();
-            parameters.put("direction", currentDirection.getValue());
-            request.put("parameters", parameters);
-            logger.info("Echo request: " + request.toString());
-            return request.toString(); 
-
-        }
-    }
-
+    // stop 
     public String returnToHeadquarters(){
         JSONObject request = new JSONObject();
         request.put("action", "stop");
@@ -130,48 +120,22 @@ public class GroundDetector {
         return request.toString();
 
     }
-
-
-    public String targetFly(){
-        return null; 
-    }
-
    
     public void processResponse(JSONObject extraInfo) {
         String found = extraInfo.optString("found", "UNKNOWN");
-        range = extraInfo.optInt("range", -1);  
 
         if ("GROUND".equals(found)){
             groundFound = true; // found ground
 
-            if (range == 0){
-                canLand = true;// can stop
-                logger.info("Ground underneath. Landing: ");
-            }
-            else{
-                // must fly to land
-                canLand = false; 
-                logger.info("Must fly to land in " + range);  
+            if (extraInfo.has("range")){
+                range = extraInfo.getInt("range");  
+                logger.info("the range is " + range);
             }
 
-            //return true; // ground found so stop checking 
         } 
-        else if ("OUT_OF_RANGE".equals(found)) { // If OUT_OF_RANGE and need to change diretions
-            System.out.println("No ground detected, area is out of range in direction: " + currentDirection);
-
-            // must change directions
-            if (mustTurn == true){
-                logger.debug("must change current Direction turning right");
-                currentDirection = currentDirection.turnRight(); 
-                logger.debug("setting must turn to true");
-                groundFound = false; 
-                canLand = false;
-
-
-                echoLeft = false; 
-                echoRight = false;
-
-            }
+        else if ("OUT_OF_RANGE".equals(found)) { // If OUT_OF_RANGE
+            logger.info("Ground out of range"); 
+            
         }
     }
 
@@ -188,9 +152,6 @@ public class GroundDetector {
 
     public boolean isGroundFound() {
         return groundFound;
-    }
-    public boolean mustDroneTurn(){
-        return mustTurn;
     }
     public boolean canDroneLand() {
         return canLand;
