c3f2b733d2eef8e592df43ba1db3682190653c24
tharnyelilvannan <tharny2005@outlook.com>
2025-03-22 19:17:45 -0400

    Removed CreekFinder and FollowCoastline

--- src/main/java/ca/mcmaster/se2aa4/island/team01/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team01/Drone.java
@@ -3,9 +3,7 @@ import ca.mcmaster.se2aa4.island.team01.Actions.Stop;
 
 public class Drone {
     private GroundDetector groundDetector;
-    private CreekFinder creekFinder;
     private FindIsland findIsland;
-    // private FollowCoastline followCoastline;
     private Stop stop;
     private SearchIsland searchIsland;
     private boolean alignedWithCoastline = false;
@@ -14,17 +12,13 @@ public class Drone {
     public Drone() {
         findIsland = new FindIsland();
         groundDetector = new GroundDetector();
-        creekFinder = new CreekFinder();
-        // followCoastline = new FollowCoastline();
         stop = new Stop();
         searchIsland = new SearchIsland();
     }
     public void updateInfo(ExtraInfo info) {
         if (info != null) {
             findIsland.updateGroundDetector(info);
-            creekFinder.updateInfo(info);
             searchIsland.updateInfo(info);
-            // followCoastline.updateInfo(info);  // Add this line
         }
     }
     
@@ -32,7 +26,7 @@ public class Drone {
         String decision;
         if (findIsland.isLandingPhase() == false) {
             decision = findIsland.searchForGround(); 
-        } else if (creekFinder.isCreekFound() == false && findIsland.isLandingPhase() == true) {
+        } else if (findIsland.isLandingPhase() == true) {
             decision = searchIsland.exploreIsland();
         } else {
             decision = stop.returnToHeadquarters();
