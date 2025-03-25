bcdab4d7b0390f38762aaf411adb08b828034922
Tharny Elilvannan <tharny2005@outlook.com>
2025-03-24 21:47:16 -0400

    Update CreekCalculator.java

--- src/main/java/ca/mcmaster/se2aa4/island/team01/CreekCalculator.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team01/CreekCalculator.java
@@ -1,13 +1,13 @@
 package ca.mcmaster.se2aa4.island.team01;
 
 public class CreekCalculator {
-    Site site = Site.get();
-    CreekList creeks = CreekList.get();
-    int creekX;
-    int creekY;
-    Creek closestCreek;
-    Double minDistance = Double.MAX_VALUE;
-    Double d;
+    private Site site = Site.get();
+    private CreekList creeks = CreekList.get();
+    private int creekX;
+    private int creekY;
+    private Creek closestCreek;
+    private Double minDistance = Double.MAX_VALUE;
+    private Double distance;
 
     public Creek calculateCreek() {
         
@@ -19,10 +19,10 @@ public class CreekCalculator {
             creekX = creek.getXCoordinate();
             creekY = creek.getYCoordinate();
 
-            d = Math.sqrt((Math.pow((siteX - creekX), 2)) + (Math.pow((siteY - creekY), 2)));
+            distance = Math.sqrt((Math.pow((siteX - creekX), 2)) + (Math.pow((siteY - creekY), 2)));
 
-            if (d < minDistance) {
-                minDistance = d;
+            if (distance < minDistance) {
+                minDistance = distance;
                 closestCreek = creek;
             }
         }
